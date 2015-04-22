e7e71a4a3035e6d283d4ade5eb6c1ffa70577c8a
JLMaksG <g_maks@ukr.net>
2015-04-22 16:50:46 +0300

    Update and rename l/admin/language/ukrainian/module/ocseo_plus.php to admin/language/ukrainian/module/ocseo_plus.php
    100%

---
+++ admin/language/ukrainian/module/ocseo_plus.php
@@ -0,0 +1,91 @@
+<?php
+// Heading
+$_['heading_title']                     = 'OcSEO Plus - Рішення в 1 клік v0.9.3.3.2 [by addist.ru]';
+$_['heading_title_routemanager']        = 'Менеджер інтеграції';
+$_['heading_title_seomanager']          = 'Менеджер ЧПУ';
+$_['heading_title_redmanager']          = 'Менеджер редиректів';
+
+// Tab
+$_['tab_seogen']                        = 'Генератор';
+
+// Text
+$_['text_module']                       = 'Модулі';
+$_['text_success']                      = 'Налаштування модуля оновлені!';
+$_['text_success_save']                 = 'Запис успішно збережено!';
+$_['text_success_delete']               = 'Вибрані записи успішно видалені!';
+$_['text_success_fix']                  = 'Дублі успішно видалені!';
+$_['text_success_seogen']               = 'ЧПУ успішно згенеровані для обраних об\'єктів!';
+$_['text_item']                         = 'Тільки елемент';
+$_['text_main']                         = 'Батьківський розділ / елемент';
+$_['text_full']                         = 'Повний шлях розділу / елемент';
+$_['text_full_manufacturers']           = 'Повний шлях бренду / елемент';
+$_['text_manufacturer']                 = 'Бренд / товар';
+$_['text_product_name']                 = 'Назва товару';
+$_['text_product_model']                = 'Модель товару';
+$_['text_product_id']                   = 'ID товару';
+$_['text_edit']                         = 'Змінити';
+$_['text_confirm']                      = 'Ви дійсно бажаєте виконати дану операцію?';
+$_['text_overwrite']                    = 'Перезаписувати';
+$_['text_not_overwrite']                = 'Не перезаписувати';
+
+// Entry
+$_['entry_status']                      = 'Статус:';
+$_['entry_translit']                    = 'Транслітерувати:';
+$_['entry_auto_keyword']                = 'Генерація ЧПУ при збереженні:';
+$_['entry_auto_keyword_h']              = 'Формування ЧПУ при створенні/редагуванні товарів, розділів, виробників та інше:';
+$_['entry_write_logs']                  = 'Записати логи 404:';
+$_['entry_suffix']                      = 'Суфікс:';
+$_['entry_suffix_category']             = 'Суфікс розділів:';
+$_['entry_suffix_brands']               = 'Суфікс сторінки, що містить виробників:';
+$_['entry_clean_url']                   = 'Чистий ЧПУ для товарів:';
+$_['entry_clean_url_h']                 = 'Видалити параметри limit, sort, order, page, tag, search з посилання товарів';
+$_['entry_category_keyword_src']        = 'Шаблон для розділів:';
+$_['entry_category_keyword_src_h']      = 'Можливі змінні:<br/>[name] - назва розділу, [id] - ID розділу';
+$_['entry_manufacturer_keyword_src']    = 'Шаблон для виробників:';
+$_['entry_manufacturer_keyword_src_h']  = 'Можливі змінні:<br/>[name] - назва виробника, [id] - ID виробника';
+$_['entry_product_keyword_src']         = 'Шаблон для товаров:';
+$_['entry_product_keyword_src_h']       = 'Можливі змінні:<br/>[name] - назва товару, [model] - модель товару, [id] - ID товару, [category_name] - назва батьківського розділу, [category_id] - ID батьківського розділу, [manufacturer_name] - назва виробника, [manufacturer_id] - ID виробника';
+$_['entry_information_keyword_src']     = 'Шаблон для статті:';
+$_['entry_information_keyword_src_h']   = 'Можливі змінні:<br/>[title] - заголовок статті, [id] - ID статті';
+$_['entry_news_keyword_src']            = 'Шаблон для новин:';
+$_['entry_news_keyword_src_h']          = 'Можливі змінні:<br/>[name] - назва новини, [id] - ID новини, [category_name] - назва батьківського розділу, [category_id] - ID батьківського розділу';
+$_['entry_category_url_tpl']            = 'Шаблон посилання розділів:';
+$_['entry_product_url_tpl']             = 'Шаблон посилання товарів:';
+$_['entry_date']                        = 'Виключити товари додані до: ';
+$_['entry_cbreadcrumb_tpl']             = 'Шаблон хлібних крихт для категорій:';
+$_['entry_pbreadcrumb_tpl']             = 'Шаблон хлібних крихт для товарів:';
+$_['entry_query']                       = 'Запит:';
+$_['entry_keyword']                     = 'ЧПУ:';
+$_['entry_oldurl']                      = 'Старе посилання:';
+$_['entry_newurl']                      = 'Нове посилання:';
+$_['entry_path']                        = 'Шлях:';
+$_['entry_route']                       = 'Роут:';
+$_['entry_argument']                    = 'Аргумент:';
+
+// Button
+$_['button_routemanager']               = 'Менеджер інтеграції';
+$_['button_seomanager']                 = 'Менеджер ЧПУ';
+$_['button_redmanager']                 = 'Менеджер редиректів';
+$_['button_filter']                     = 'Фільтр';
+$_['button_check']                      = 'Перевірити дублікати';
+$_['button_clear']                      = 'Очистити пошук';
+$_['button_back']                       = 'Назад';
+$_['button_fix']                        = 'Позбутися дублів';
+$_['button_insert']                     = 'Додати';
+
+// Column
+$_['column_path']                       = 'Шлях';
+$_['column_route']                      = 'Роут';
+$_['column_query']                      = 'Запит';
+$_['column_keyword']                    = 'ЧПУ';
+$_['column_oldurl']                     = 'Старе посилання';
+$_['column_newurl']                     = 'Нове посилання';
+$_['column_action']                     = 'Дія';
+$_['column_argument']                   = 'Аргумент';
+
+// Error
+$_['error_config']                      = 'ЧПУ не включений в настройках системи, будь ласка, увімкніть його!';
+$_['error_htaccess']                    = 'Не знайдений файл .htaccess в корені сайту, перевірте це!';
+$_['error_exists']                      = 'Введені значення не унікальні!';
+$_['error_form']                        = 'Перевірте форму на помилки!';
+$_['error_not_found']                   = 'Такий запис не знайдено!';
